656a71979ba1e93f3d2a55f376acc737ab4a91ea
marvelman3284 <jedimaster3284@gmail.com>
2022-01-17 21:59:43 -0500

    add rofi shit

---
+++ .config/rofi/applets/menu/quicklinks.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+style="$($HOME/.config/rofi/applets/menu/style.sh)"
+
+dir="$HOME/.config/rofi/applets/menu/configs/$style"
+rofi_command="rofi -theme $dir/quicklinks.rasi"
+
+# Error msg
+msg() {
+	rofi -theme "$HOME/.config/rofi/applets/styles/message.rasi" -e "$1"
+}
+
+# Browser
+if [[ -f /usr/bin/firefox ]]; then
+	app="firefox"
+elif [[ -f /usr/bin/chromium ]]; then
+	app="chromium"
+elif [[ -f /usr/bin/midori ]]; then
+	app="midori"
+else
+	msg "No suitable web browser found!"
+	exit 1
+fi
+
+# Links
+ddg=""
+stackoverflow=""
+github=""
+mail=""
+youtube=""
+
+# Variable passed to rofi
+options="$ddg\n$stackoverflow\n$github\n$mail\n$youtube"
+
+chosen="$(echo -e "$options" | $rofi_command -p "Open In  :  $app" -dmenu -selected-row 0)"
+case $chosen in
+    $ddg)
+        $app https://www.duckduckgo.com &
+        ;;
+    $stackoverflow)
+        $app https://www.stackoverflow.com &
+        ;;
+    $github)
+        $app https://www.github.com &
+        ;;
+    $mail)
+        $app https://www.gmail.com &
+        ;;
+    $youtube)
+        $app https://www.youtube.com &
+        ;;
+esac
+
