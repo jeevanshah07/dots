5b89582c5ee71a374a7df7eec67b90023040b75f
marvelman3284 <jedimaster3284@gmail.com>
2022-07-15 14:47:25 -0400

    add markdown preview plugin to neovim

--- .config/nvim/custom/plugins/init.lua
+++ .config/nvim/custom/plugins/init.lua
@@ -93,4 +93,10 @@ return {
       require("custom.plugins.todo")
     end
   },
+
+  [ "iamcco/markdown-preview.nvim"] = {
+    run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown"
+    },
+  },
+
 }
