1812bc435d0ac77a49503fc9cea96466f1df446e
Jeevan Shah <jedimaster3284@gmail.com>
2026-01-13 15:15:57 -0500

    update nvim dots

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,22 +1,15 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
 local nvlsp = require "nvchad.configs.lspconfig"
 
 local servers = { "clangd", "ts_ls", "pyright", "html", "cssls", "ruff", "texlab" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  vim.lsp.config(lsp, {
     on_attach = nvlsp.on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
-  }
+  })
+  vim.lsp.enable(lsp)
 end
-
-lspconfig.pyright.setup({
-  on_attach = function(client, bufnr)
-    -- Disable the specific mapping in insert mode
-    vim.api.nvim_buf_set_keymap(bufnr, 'i', ',<space>', ',<space>', { noremap = true, silent = true })
-  end
-})
